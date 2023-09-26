f1e4668cd2e0afc5c777a32e947cafd2bc797840
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-09-26 16:53:46 +0530

    useContext

---
+++ src/ComC.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ComB from "./ComB";
+
+
+const ComC=()=>{
+    return(
+        <>
+        <h1>
+            My name is Akash
+        </h1>;
+        </>
+    );
+};
+
+export default ComC;
---
+++ src/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact=()=>{
+  return(
+    <>
+    <h1>Hello, I am a Contact Page</h1>
+
+    </>
+  )
+};
+
+export default Contact;
